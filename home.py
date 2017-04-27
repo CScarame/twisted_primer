2f3944159f0a6f7ebf29bc6eb7546ecb653df47b
Chris 'Scary' Scaramella <CScarame@nd.edu>
2017-04-27 00:39:08 -0400

    added Wednesday stuff

--- home.py
+++ home.py
@@ -1,22 +1,53 @@
-from twisted.internet.protocol import ClientFactory
+from twisted.internet.protocol import Factory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
+import sys
 
-class MyConnection(Protocol):
+
+class InputConnection(Protocol):
 
     def connectionMade(self):
-        print "new connection made" 
+        print "Sending data:"
+        self.transport.write("This is the data")
+
+    def dataReceived(self, data):
+        print data
+        n = raw_input(">")
+        if n == "exit":
+            reactor.stop()
+            
+        else:
+            self.transport.write(n)
+        
+class Add1Connection(Protocol):
+
+    def connectionMade(self):
+        print "Connection Made"
+        self.transport.write("1")
+
+    def dataReceived(self,data):
+        print data
+        self.transport.write(str(int(data) + 1))
 
 
+class CommandConnection(Protocol):
 
-class MyConnectionFactory(ServerFactory):
-    def __init__(self):
-        self.myconn = MyConnection()
+    def connectionMade(self):
+        print "Command Connection Received"
 
+class HomeConnectionFactory(Factory):
+    def __init__(self, connection_type):
+        self.connection_type = connection_type
+        self.input_conn = InputConnection()
+        self.add1_conn = Add1Connection()
+	self.command_conn = CommandConnection()
     def buildProtocol(self, addr):
-        return self.myconn
-
-
-reactor.listenTCP( 40001, MyConnectionFactory())
+        if self.connection_type == "input":
+            return self.input_conn
+        elif self.connection_type == "add1":
+            return self.add1_conn
+        elif self.connection_type == "command":
+	    return self.command_conn
+reactor.listenTCP( 40052, HomeConnectionFactory("command"))
 
 reactor.run()
--- work.py
+++ work.py
@@ -5,18 +5,36 @@ from twisted.internet import reactor
 class MyConnection(Protocol):
 
     def connectionMade(self):
-        print "new connection made" 
+        print "new connection made"
+
+    def dataReceived(self,data):
+        print data
+        if data == "quit":
+            reactor.stop()
+        else:
+        	self.transport.write(data)
+    def connectionLost(self, reason):
+	print "connection lost"
+
+class CommandConnection(Protocol):
+    def connectionMade(self):
+        print "command connection made"
 
 
+class DataConnection(Protocol):
+    
 
-class MyConnectionFactory(ClientFactory):
-    def __init__(self):
-        self.myconn = MyConnection()
-
+class WorkConnectionFactory(ClientFactory):
+    def __init__(self, connection_type):
+	self.connection_type = connection_type
+	self.myconn = MyConnection()
+        self.command_conn = CommandConnection()
     def buildProtocol(self, addr):
-        return self.myconn
+        if self.connection_type = "test":
+            return self.myconn
+        elif self.connection_type = "command":
+            return self.command_conn
 
-
-reactor.connectTCP("10.25.247.41", 40001, MyConnectionFactory())
+reactor.connectTCP("localhost", 40052, MyConnectionFactory("command"))
 
 reactor.run()
