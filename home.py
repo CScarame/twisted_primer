2c740325ef5df18742db265d4c14f8a3414f3f35
Chris Scaramella <CScarame@nd.edu>
2017-05-01 05:42:56 -0400

    Finally working!

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from twisted.internet.protocol import Factory
+from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
 import sys
@@ -18,39 +18,65 @@ class InputConnection(Protocol):
             
         else:
             self.transport.write(n)
-        
-class Add1Connection(Protocol):
-
-    def connectionMade(self):
-        print "Connection Made"
-        self.transport.write("1")
-
-    def dataReceived(self,data):
-        print data
-        self.transport.write(str(int(data) + 1))
 
 class CommandConnection(Protocol):
     def connectionMade(self):
         print "Command Connection Received"
+        # wait for client to connect
+        reactor.listenTCP(42052, HomeConnectionFactory("client", self))
+    def dataReceived(self, data):
+        print "command: ",
+        print data
 
 class ClientConnection(Protocol):
     def __init__(self, command_conn):
         self.command_conn = command_conn
+        self.data_conn = None
+        self.data = None
     def connectionMade(self):
         print "Client Connection Established"
-
+        # now set up data connection
+        reactor.listenTCP(41052, HomeConnectionFactory("data", self.command_conn, self))
+        self.command_conn.transport.write("connect")
+    def dataReceived(self, data):
+        print "client: ",
+        print data
+        if self.data_conn:
+            self.data_conn.transport.write(data)
+        else:
+            print "client: Storing data until data connection finishes"
+            
+    def passData(self, data_connection):
+        self.data_conn = data_connection
+        if self.data:
+            print "client: Sending data"
+        else:
+            print "client: Found data connection"
+        
 class DataConnection(Protocol):
-    def __init__(self, command_conn):
+    def __init__(self, command_conn, client_conn):
         self.command_conn = command_conn
+        self.client_conn = client_conn
     def connectionMade(self):
         print "Data Connection Established"
+        self.client_conn.passData(self)
+    def dataReceived(self,data):
+        print "data: ",
+        print data
+        self.client_conn.transport.write(data)
 
-class HomeConnectionFactory(Factory):
-    def __init__(self, connection_type):
+class HomeConnectionFactory(ClientFactory):
+    def __init__(self, connection_type, command = None, client = None):
         self.connection_type = connection_type
-	self.command_conn = CommandConnection()
-        self.client_conn  = ClientConnection(self.command_conn)
-        self.data_conn    = DataConnection(self.command_conn)
+        if command:
+            self.command_conn = command
+        else:
+            self.command_conn = CommandConnection()
+        if client:
+            self.client_conn = client
+        else:
+            self.client_conn  = ClientConnection(self.command_conn)
+        self.data_conn    = DataConnection(self.command_conn, self.client_conn)
     def buildProtocol(self, addr):
         if self.connection_type == "command":
 	    return self.command_conn
@@ -58,6 +84,8 @@ class HomeConnectionFactory(Factory):
             return self.client_conn
         elif self.connection_type == "data":
             return self.data_conn
-reactor.listenTCP( 40052, HomeConnectionFactory("command"))
+
+if __name__ == "__main__":
+    reactor.listenTCP( 40052, HomeConnectionFactory("command"))
 
 reactor.run()
--- work.py
+++ work.py
@@ -1,6 +1,7 @@
 from twisted.internet.protocol import ClientFactory
 from twisted.internet.protocol import Protocol
 from twisted.internet import reactor
+from twisted.internet.defer import DeferredQueue
 
 class MyConnection(Protocol):
 
@@ -19,23 +20,60 @@ class MyConnection(Protocol):
 class CommandConnection(Protocol):
     def connectionMade(self):
         print "Command Connection Established"
+    def dataReceived(self,data):
+        print "command: ",
+        print data
+        if data == "connect":
+            print "Connecting Data"
+            reactor.connectTCP("10.25.247.41", 41052, WorkConnectionFactory("data",self))
 
 class DataConnection(Protocol):
+    def __init__(self,command):
+        self.command_conn = command
+        self.service = None
+        self.data = DeferredQueue()
     def connectionMade(self):
         print "Data Connection Established"
+        reactor.connectTCP("localhost", 22, WorkConnectionFactory("service", self.command_conn, self))
+    def dataReceived(self,data):
+        print "data: ",
+        print data
+        self.data.put(data)
+    def setService(self, service):
+        self.service_conn = service
+        print "Service Connected to data"
+        self.data.get().addCallback(self.serveData)
+    def serveData(self, data):
+        print "Data SERVED!"
+        self.service_conn.transport.write(data)
+        self.data.get().addCallback(self.serveData)
 
 class ServiceConnection(Protocol):
+    def __init__(self, command, data):
+        self.command_conn = command
+        self.data_conn = data
     def connectionMade(self):
         print "Service Connection Established"
+        self.data_conn.setService(self)
+    def dataReceived(self,data):
+        print "service: ",
+        print data
+        self.data_conn.transport.write(data)
         
 
 class WorkConnectionFactory(ClientFactory):
-    def __init__(self, connection_type):
+    def __init__(self, connection_type, command_connection = None, data_connection = None):
 	self.connection_type = connection_type
 	self.myconn = MyConnection()
-        self.command_conn = CommandConnection()
-        self.data_conn    = DataConnection()
-        self.service_conn = ServiceConnection()
+        if command_connection:
+            self.command_conn = command_connection
+        else:
+            self.command_conn = CommandConnection()
+        if data_connection:
+                self.data_conn = data_connection
+        else:
+            self.data_conn    = DataConnection(self.command_conn)
+        self.service_conn = ServiceConnection(self.command_conn, self.data_conn)
     def buildProtocol(self, addr):
         if self.connection_type == "test":
             return self.myconn
@@ -46,6 +84,7 @@ class WorkConnectionFactory(ClientFactory):
         elif self.connection_type == "service":
             return self.service_conn
 
-reactor.connectTCP("10.25.247.41", 40052, WorkConnectionFactory("command"))
+if __name__ == "__main__":
+    reactor.connectTCP("10.25.247.41", 40052, WorkConnectionFactory("command"))
 
 reactor.run()
